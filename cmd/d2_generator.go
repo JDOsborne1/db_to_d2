f4bf6b1ab7377299a423068b809e961a8131317b
Joseph Osborne <jd.osborne5@icloud.com>
2023-03-15 21:33:38 +0000

    removing illegal references metadata in d2 builder

--- cmd/d2_generator.go
+++ cmd/d2_generator.go
@@ -18,21 +18,7 @@ func generateD2Diagram(schema Schema) string {
             if column.Key == "PRI" {
                 builder.WriteString(" {constraint: primary_key}")
             }
-            
-            if column.Reference != nil {
-                builder.WriteString(fmt.Sprintf(" {reference: %s.%s", column.Reference.Table, column.Reference.Column))
-                
-                if column.Reference.OnDelete != "" {
-                    builder.WriteString(fmt.Sprintf(", on_delete: %s", column.Reference.OnDelete))
-                }
-                
-                if column.Reference.OnUpdate != "" {
-                    builder.WriteString(fmt.Sprintf(", on_update: %s", column.Reference.OnUpdate))
-                }
-                
-                builder.WriteString("}")
-            }
-            
+                                  
             builder.WriteString("\n")
         }
         
