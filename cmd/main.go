d990ff7498bcb8db5cf22a4bf0a8836b5fba610d
Joseph Osborne <jd.osborne5@icloud.com>
2023-04-09 14:20:31 +0100

    Introduce Env Var Driven Overrides

--- cmd/main.go
+++ cmd/main.go
@@ -72,16 +72,24 @@ type TableGroup struct {
 	Tables []string `json:"tables,omitempty"`
 }
 
-func get_virtual_links () []VirtualLink {
+func get_virtual_links() []VirtualLink {
 	links := []VirtualLink{}
-	links_json, _ := os.ReadFile("example_virtual_links.json")
+	links_json, err := os.ReadFile(os.Getenv("VIRTUAL_LINKS_PATH"))
+	if err != nil {
+		//TODO: Log error, or bubble up instead of printing to console
+		fmt.Println("Failed to read virtual links file")
+	}
 	json.Unmarshal(links_json, &links)
 	return links
 }
 
 func get_table_groups() []TableGroup {
 	table_groups := []TableGroup{}
-	table_groups_json, _ := os.ReadFile("example_table_groups.json")
+	table_groups_json, err := os.ReadFile(os.Getenv("TABLE_GROUPS_PATH"))
+	if err != nil {
+		//TODO: Log error, or bubble up instead of printing to console
+		fmt.Println("Failed to read table groups file")
+	}
 	json.Unmarshal(table_groups_json, &table_groups)
 	return table_groups
 }
--- connection_vars.sh
+++ connection_vars.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# connection vars
 export D2_TARGET_DB_USER="root"
 export D2_TARGET_DB_PASSWORD="example_password"
 export D2_TARGET_DB_HOST="localhost"
 export D2_TARGET_DB_PORT="3306"
 export D2_TARGET_DB_NAME="testdb"
+
+# override vars
+export TABLE_GROUPS_PATH="example_table_groups.json"
+export VIRTUAL_LINKS_PATH="example_virtual_links.json"
