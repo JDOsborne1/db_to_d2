290f09feca30711434161bffadee0dc54c93a996
Joseph Osborne <jd.osborne5@icloud.com>
2023-04-09 15:54:34 +0100

    Add Minimal Tests to Restrict Function

---
+++ cmd/restrictions_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+//TestRestrictionsIdentity is a basic test that checks that the identity function returns the same value as the input.
+func TestRestrictionsIdentity(t *testing.T) {
+	//Test data
+	input := Schema{
+		Tables: []Table{
+			{
+				Name: "test",
+				Columns: []Column{
+					{
+						Name: "test",
+					},
+				},
+			},
+		},
+	}
+	expected := input
+
+	//Execute test
+	actual := restrict(input, standard)
+
+	//Compare actual to expected
+	if !reflect.DeepEqual(actual, expected) {
+		t.Log("Identity function failed to return the same value as the input.")
+		t.Fail()
+	}
+}
