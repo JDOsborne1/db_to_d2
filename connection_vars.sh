14069f0ef0431b10cb71ee69ac2a5739703dc45a
Joseph Osborne <jd.osborne5@icloud.com>
2023-04-10 21:06:47 +0100

    Reorder Env Vars To Put Connections Together

--- connection_vars.sh
+++ connection_vars.sh
@@ -6,6 +6,7 @@ export D2_TARGET_DB_PASSWORD="example_password"
 export D2_TARGET_DB_HOST="localhost"
 export D2_TARGET_DB_PORT="3306"
 export D2_TARGET_DB_NAME="testdb"
+export D2_TARGET_DB_TYPE="mysql" # accepts mysql
 
 # override vars
 export TABLE_GROUPS_PATH="example_table_groups.json"
@@ -13,7 +14,6 @@ export VIRTUAL_LINKS_PATH="example_virtual_links.json"
 export DESIGNATED_USER="'testuser'@'%'"
 
 # config vars
-export D2_TARGET_DB_TYPE="mysql" # accepts mysql
 export VIRTUAL_LINKS="true"
 export TABLE_GROUPS="true"
 export RESTRICTOR_TYPE="minimal" # accepts user or minimal
