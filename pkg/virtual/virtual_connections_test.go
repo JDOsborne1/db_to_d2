a395e71912e7a3e7a44d349f1bc8b7771c50b0e7
Joseph Osborne <jd.osborne5@icloud.com>
2023-04-13 10:55:40 +0100

    Add Test To Virtual Connections

---
+++ pkg/virtual/virtual_connections_test.go
@@ -0,0 +1,88 @@
+package virtual
+
+import (
+	"core"
+	"reflect"
+	"testing"
+)
+
+// Test that the virtual links are added to the schema
+func TestAugment_schema(t *testing.T) {
+	inital_schema := core.Schema{
+		Tables: []core.Table{
+			{
+				Name: "table1",
+				Columns: []core.Column{
+					{
+						Name: "col1",
+					},
+					{
+						Name: "col2",
+					},
+				},		
+			},
+			{
+				Name: "table2",
+				Columns: []core.Column{
+					{
+						Name: "col1",
+					},
+					{
+						Name: "col2",
+					},
+				},
+			},
+		},
+	}
+	experimental_links := []VirtualLink{
+		{
+			SourceTable: "table1",
+			SourceColumn: "col1",
+			ReferencedTable: "table2",
+			ReferencedColumn: "col1",
+		},
+	}
+
+	expected := core.Schema{
+		Tables: []core.Table{
+			{
+				Name: "table1",
+				Columns: []core.Column{
+					{
+						Name: "col1",
+						Key: "VIRTUAL",
+						Reference: &core.Reference{
+							Table: "table2",
+							Column: "col1",
+						},
+						Extra: "Virtual link to table2.col1",
+					},
+					{
+						Name: "col2",
+					},
+				},		
+			},
+			{
+				Name: "table2",
+				Columns: []core.Column{
+					{
+						Name: "col1",
+						Key: "VIRTUAL",
+						Extra: "Virtual link from table1.col1",
+					},
+					{
+						Name: "col2",
+					},
+				},
+			},
+		},
+	}
+
+	actual := Augment_schema(inital_schema, experimental_links)
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+
+
+}
