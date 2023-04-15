96571b6cb38756f1ecf68c514024819fb68f94ac
Joseph Osborne <jd.osborne5@icloud.com>
2023-04-15 21:02:59 +0100

    Extend Tests to Cover Minimalist Restrictions

--- pkg/core/restrictions_test.go
+++ pkg/core/restrictions_test.go
@@ -6,75 +6,109 @@ import (
 )
 
 var example_schema = Schema{
-    Tables: []Table{
-        {
-            Name: "users",
-            Columns: []Column{
-                {
-                    Name:     "id",
-                    Type:     "int",
-                    Nullable: false,
-                    Key:      "PRIMARY KEY",
-                    Extra:    "AUTO_INCREMENT",
-                },
-                {
-                    Name:     "username",
-                    Type:     "varchar(255)",
-                    Nullable: false,
-                    Key:      "UNIQUE KEY",
-                    Extra:    "",
-                },
-                {
-                    Name:     "email",
-                    Type:     "varchar(255)",
-                    Nullable: false,
-                    Key:      "UNIQUE KEY",
-                    Extra:    "",
-                },
-            },
-        },
-        {
-            Name: "posts",
-            Columns: []Column{
-                {
-                    Name:     "id",
-                    Type:     "int",
-                    Nullable: false,
-                    Key:      "PRIMARY KEY",
-                    Extra:    "AUTO_INCREMENT",
-                },
-                {
-                    Name:     "title",
-                    Type:     "varchar(255)",
-                    Nullable: false,
-                    Key:      "",
-                    Extra:    "",
-                },
-                {
-                    Name:     "content",
-                    Type:     "text",
-                    Nullable: true,
-                    Key:      "",
-                    Extra:    "",
-                },
-                {
-                    Name:     "user_id",
-                    Type:     "int",
-                    Nullable: false,
-                    Key:      "FOREIGN KEY",
-                    Extra:    "",
-                    Reference: &Reference{
-                        Table:    "users",
-                        Column:   "id",
-                        OnDelete: "CASCADE",
-                        OnUpdate: "CASCADE",
-                    },
-                },
-            },
-        },
-    },
+	Tables: []Table{
+		{
+			Name: "users",
+			Columns: []Column{
+				{
+					Name:     "id",
+					Type:     "int",
+					Nullable: false,
+					Key:      "PRIMARY KEY",
+					Extra:    "AUTO_INCREMENT",
+				},
+				{
+					Name:     "username",
+					Type:     "varchar(255)",
+					Nullable: false,
+					Key:      "UNIQUE KEY",
+					Extra:    "",
+				},
+				{
+					Name:     "email",
+					Type:     "varchar(255)",
+					Nullable: false,
+					Key:      "UNIQUE KEY",
+					Extra:    "",
+				},
+			},
+		},
+		{
+			Name: "posts",
+			Columns: []Column{
+				{
+					Name:     "id",
+					Type:     "int",
+					Nullable: false,
+					Key:      "PRIMARY KEY",
+					Extra:    "AUTO_INCREMENT",
+				},
+				{
+					Name:     "title",
+					Type:     "varchar(255)",
+					Nullable: false,
+					Key:      "",
+					Extra:    "",
+				},
+				{
+					Name:     "content",
+					Type:     "text",
+					Nullable: true,
+					Key:      "",
+					Extra:    "",
+				},
+				{
+					Name:     "user_id",
+					Type:     "int",
+					Nullable: false,
+					Key:      "FOREIGN KEY",
+					Extra:    "",
+					Reference: &Reference{
+						Table:    "users",
+						Column:   "id",
+						OnDelete: "CASCADE",
+						OnUpdate: "CASCADE",
+					},
+				},
+			},
+		},
+		{
+			Name: "meta",
+			Columns: []Column{
+				{
+					Name:     "meta_key",
+					Type:     "varchar(255)",
+					Nullable: false,
+					Key:      "",
+					Extra:    "",
+				},
+				{
+					Name:     "meta_value",
+					Type:     "varchar(255)",
+					Nullable: false,
+					Key:      "",
+					Extra:    "",
+				},
+			},
+		},
+	},
 }
 
+func get_table_names(_input Schema) []string {
+	var output []string
+	for _, table := range _input.Tables {
+		output = append(output, table.Name)
+	}
+	return output
+}
+
+func get_column_names(_input Table) []string {
+	var output []string
+	for _, column := range _input.Columns {
+		output = append(output, column.Name)
+	}
+	return output
+}
 
 // TestRestrictionsIdentity is a basic test that checks that the identity function returns the same value as the input.
 func TestRestrictionsIdentity(t *testing.T) {
@@ -86,4 +120,26 @@ func TestRestrictionsIdentity(t *testing.T) {
 		t.Log("Identity function failed to return the same value as the input.")
 		t.Fail()
 	}
+}
+
+func TestRestrictionsMinimalist(t *testing.T) {
+	actual := Restrict(example_schema, Minimalist)
+	names := get_table_names(actual)
+	if !(equal_set(names, []string{"users", "posts"})) {
+		t.Log("Minimalist cleared inappropriate tables.")
+		t.Fail()
+	}
+
+	if !(equal_set(get_column_names(actual.Tables[0]), []string{"id", "username", "email"})) {
+		t.Log("Minimalist cleared columns in a table where all the columns are keys.")
+		t.Fail()
+	}
+
+	if !(equal_set(get_column_names(actual.Tables[1]), []string{"id", "user_id"})) {
+		t.Log("Minimalist didn't clear the right columns in a table which has partial keys.")
+		t.Fail()
+	}
+
+	t.Log(actual.Tables)
+	t.Fail()
 }
--- pkg/core/utilities.go
+++ pkg/core/utilities.go
@@ -9,6 +9,18 @@ func in_set(_element string, _set []string) bool {
 	return false
 }
 
+func equal_set(_set1 []string, _set2 []string) bool {
+	if len(_set1) != len(_set2) {
+		return false
+	}
+	for _, element := range _set1 {
+		if !in_set(element, _set2) {
+			return false
+		}
+	}
+	return true
+}
+
 // Wraps a table name in a group tag if it is in a group. Otherwise, returns the table name.
 // This is used to ensure that links in the d2 graph are drawn correctly when table groups are used.
 func wrap_name_in_group(_table_name string, _grouping []TableGroup) string {
