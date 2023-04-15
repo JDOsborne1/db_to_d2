1a674dc4b5703a325043649ffa901f075927411d
Joseph Osborne <jd.osborne5@icloud.com>
2023-04-15 20:40:10 +0100

    Use a More Full Featured Test Schema
    This extends the restrictions test schema with some
    more details, so that it can be used to test more
    kinds of restriction

--- pkg/core/restrictions_test.go
+++ pkg/core/restrictions_test.go
@@ -5,28 +5,84 @@ import (
 	"testing"
 )
 
+var example_schema = Schema{
+    Tables: []Table{
+        {
+            Name: "users",
+            Columns: []Column{
+                {
+                    Name:     "id",
+                    Type:     "int",
+                    Nullable: false,
+                    Key:      "PRIMARY KEY",
+                    Extra:    "AUTO_INCREMENT",
+                },
+                {
+                    Name:     "username",
+                    Type:     "varchar(255)",
+                    Nullable: false,
+                    Key:      "UNIQUE KEY",
+                    Extra:    "",
+                },
+                {
+                    Name:     "email",
+                    Type:     "varchar(255)",
+                    Nullable: false,
+                    Key:      "UNIQUE KEY",
+                    Extra:    "",
+                },
+            },
+        },
+        {
+            Name: "posts",
+            Columns: []Column{
+                {
+                    Name:     "id",
+                    Type:     "int",
+                    Nullable: false,
+                    Key:      "PRIMARY KEY",
+                    Extra:    "AUTO_INCREMENT",
+                },
+                {
+                    Name:     "title",
+                    Type:     "varchar(255)",
+                    Nullable: false,
+                    Key:      "",
+                    Extra:    "",
+                },
+                {
+                    Name:     "content",
+                    Type:     "text",
+                    Nullable: true,
+                    Key:      "",
+                    Extra:    "",
+                },
+                {
+                    Name:     "user_id",
+                    Type:     "int",
+                    Nullable: false,
+                    Key:      "FOREIGN KEY",
+                    Extra:    "",
+                    Reference: &Reference{
+                        Table:    "users",
+                        Column:   "id",
+                        OnDelete: "CASCADE",
+                        OnUpdate: "CASCADE",
+                    },
+                },
+            },
+        },
+    },
+}
+
+
 // TestRestrictionsIdentity is a basic test that checks that the identity function returns the same value as the input.
 func TestRestrictionsIdentity(t *testing.T) {
-	//Test data
-	input := Schema{
-		Tables: []Table{
-			{
-				Name: "test",
-				Columns: []Column{
-					{
-						Name: "test",
-					},
-				},
-			},
-		},
-	}
-	expected := input
-
 	//Execute test
-	actual := Restrict(input, Standard)
+	actual := Restrict(example_schema, Standard)
 
 	//Compare actual to expected
-	if !reflect.DeepEqual(actual, expected) {
+	if !reflect.DeepEqual(actual, example_schema) {
 		t.Log("Identity function failed to return the same value as the input.")
 		t.Fail()
 	}
