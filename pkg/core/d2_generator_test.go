e006c399c72a8a8867bc68f725f089d7a12ef0c9
Joseph Osborne <jd.osborne5@icloud.com>
2023-04-15 21:47:30 +0100

    Include Simple Formatter For Test Cases
    This is not the same as a full d2 formatter, but
    serves the purpose of making writing test
    examples not dependent on formatting.

--- pkg/core/d2_generator_test.go
+++ pkg/core/d2_generator_test.go
@@ -2,12 +2,19 @@ package core
 
 // The d2 tests can be done by using strings.TrimSpace() to remove the whitespace from the output of the d2 generator.
 // This should remove the influence of formatting differences between the two strings. This is a simple way to test that that two are equal.
-// In future, we may want to use a d2 formatter to make the output more consistent.
 
 import (
 	"strings"
 	"testing"
+
 )
+
+func simple_formatter(_input string) string {
+	_input = strings.Replace(_input, "\t", "", -1)
+	_input = strings.Replace(_input, "\n\n", "\n", -1)
+	_input = strings.TrimSpace(_input)
+	return _input 
+}
 
 func Test_d2_generator(t *testing.T) {
 	schema := Schema{
@@ -41,23 +48,27 @@ func Test_d2_generator(t *testing.T) {
 		},
 	}
 
-expected := `table1: {
-  shape: sql_table
+expected := `
+table1: {
+  		shape: sql_table
   col1: int
-  col2: varchar
+  		col2: varchar
 }
 
 table2: {
   shape: sql_table
   col1: int
-  col2: varchar
-}`
-	expected = strings.TrimSpace(expected)
 
-	actual := strings.TrimSpace(Schema_to_d2(schema, []TableGroup{}))
+  col2: varchar
+}
+`
+	expected = simple_formatter(expected)
+
+	actual := simple_formatter(Schema_to_d2(schema, []TableGroup{}))
 
 	if actual != expected {
 		t.Errorf("Expected \n%s\n, got \n%s", expected, actual)
 	}
 
 }
+
